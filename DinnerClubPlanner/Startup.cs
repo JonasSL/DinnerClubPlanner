96799b32085f9cd7332e5a02230caf65881038dc
JonasSL <jonaslarsen1994@gmail.com>
2017-01-09 12:50:57 +0100

    Add project files.

---
+++ DinnerClubPlanner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DinnerClubPlanner.Startup))]
+namespace DinnerClubPlanner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
